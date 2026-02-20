fa55e223151abc3158731f3135489347a9558853
AmardeepKhera <amardeep.singhkhera@virginaustralia.com>
2026-02-20 15:07:20 +1100

    setup commit

---
+++ src/main/kotlin/com/au/swarin/selective_tests/SelectiveTestsApplication.kt
@@ -0,0 +1,11 @@
+package com.au.swarin.selective_tests
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SelectiveTestsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SelectiveTestsApplication>(*args)
+}
